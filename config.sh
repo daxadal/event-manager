67c40aed914f913a9bcd798cd01bbc22f8a1780a
daxadal <e.garciadececa@gmail.com>
2021-05-24 16:11:01 +0200

    Initial commit

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+npm install --save-dev eslint prettier
+#npx install-peerdeps --dev eslint-config-airbnb
+npm install --save-dev eslint-config-airbnb@18.2.1 eslint@^7.2.0 eslint-plugin-import@^2.22.1
+#npm install --save-dev eslint-plugin-jsx-a11y@^6.4.1 eslint-plugin-react@^7.21.5 eslint-plugin-react-hooks@^1.7.0
+npm install --save-dev eslint-config-prettier eslint-plugin-prettier

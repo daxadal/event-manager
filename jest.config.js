bbedebb3c5efb6af7eb04a8d2b069b5a888a46b9
eric <eric@hamelyn.com>
2021-12-02 15:28:50 +0100

    Install and config Jest (and supertest and jest-junit)

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  reporters: ['default', 'jest-junit'],
+
+  roots: ['./test'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|js)'],
+  transform: {
+    '^.+\\.(ts|js)$': 'ts-jest',
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  testTimeout: 10000,
+};

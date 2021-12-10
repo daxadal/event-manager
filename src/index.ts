cf5f3082d8521035e65c5a5ebb4c4a8170ccc0bd
eric <eric@hamelyn.com>
2021-12-10 13:38:29 +0100

    Extract express app in a new file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import 'module-alias/register';
 
-import express from 'express';
-import rateLimit from 'express-rate-limit';
-
-const app = express();
-
-import eventsApp from '@/routes/events';
-import usersApp from '@/routes/users';
-import devApp from '@/routes/dev';
-
 import { api, bree as breeConfig, configDebug, socket } from '@/config';
 import bree from '@/scheduler';
+import socketServer from '@/socket';
+import app from '@/app';
 import * as DB from '@/services/db';
 
 console.info('=== SERVER STARTUP ===');
@@ -35,43 +28,6 @@ if (configDebug.parsingErrors.length > 0) {
 }
 
 DB.setup();
-
-import socketServer, { sendReminders } from '@/socket';
-import { checkBreeToken } from '@/services/auth';
-
-if (api.DEV) app.use('/dev', devApp);
-app.use('/events', eventsApp);
-app.use('/users', usersApp);
-
-const MAIN_RPM = 10;
-
-app.use(
-  rateLimit({
-    max: MAIN_RPM,
-    windowMs: 60 * 1000, // 1 minute
-    message: 'Too many requests',
-  })
-);
-
-app.post('/jobs/remind', checkBreeToken, async (req: any, res) => {
-  try {
-    console.info('Remind:', req.dates);
-    const { start, end } = req.dates;
-
-    const events = await DB.Event.find({
-      startDate: { $gte: start, $lte: end },
-    });
-    await sendReminders(events);
-    res.status(200).send({ message: 'Reminders sent' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ error: 'Internal server error' });
-  }
-});
-
-app.use((req, res) => {
-  res.status(404).send({ error: 'Endpoint not found' });
-});
 
 app.listen(api.PORT, () => {
   console.info(`Server listening on port ${api.PORT}...`);

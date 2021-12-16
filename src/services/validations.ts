e7b9f5df48875e1ac17079caadaf747e995ace5c
eric <eric@hamelyn.com>
2021-12-16 12:11:39 +0100

    Created Joi middleware

---
+++ src/services/validations.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import Joi, { LanguageMessages } from "joi";
+import { Logger } from "winston";
+
+const validate =
+  (fieldToValidate: "body" | "query" | "params") =>
+  (schema: Joi.Schema, messages?: LanguageMessages): RequestHandler =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const logger : Logger | Console = (req as any).logger || console;
+    const { value, error } = schema.validate(req[fieldToValidate], {messages});
+    if (error) {
+      logger.info(`"${req.originalUrl}": ${fieldToValidate} failed validation:\n -  Error: ${error.message}`);
+      logger.debug(`Full Joi error:`, error);
+      res.status(400).send({ message: error.message });
+    } else {
+      req[fieldToValidate] = value;
+      next();
+    }
+  };
+
+export const validateBody = validate("body");
+export const validateQuery = validate("query");
+export const validatePath = validate("params");

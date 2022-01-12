4a2ba84350e459d30b7e43e97b494f9d14b46953
eric <eric@hamelyn.com>
2022-01-12 13:32:19 +0100

    Change config import on Socket Client

--- test/mocks/socket-client.ts
+++ test/mocks/socket-client.ts
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
-import * as config from "@/config";
+import { socket as socketConfig } from "@/config";
 import { getLogger } from "@/services/winston";
 
 const logger = getLogger("mocks/socket-client");
 
 export const createSocketClient = () => {
-  const socket = io(`http://localhost:${config.socket.PORT}`);
+  const socket = io(`http://localhost:${socketConfig.PORT}`);
 
   socket.on("connect", () => {
     logger.debug("Client: Connection made to server: ", socket.id);

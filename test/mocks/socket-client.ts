80c2d7ed509c3eeeec4000e70a546f36b1370352
eric <eric@hamelyn.com>
2022-01-12 13:30:49 +0100

    Use winston logger on Socket Client

--- test/mocks/socket-client.ts
+++ test/mocks/socket-client.ts
@@ -1,43 +1,44 @@
 import { io } from "socket.io-client";
 
 import * as config from "@/config";
+import { getLogger } from "@/services/winston";
+
+const logger = getLogger("mocks/socket-client");
 
 export const createSocketClient = () => {
   const socket = io(`http://localhost:${config.socket.PORT}`);
 
-  if (config.mocha.SOCKET_VERBOSE) {
-    socket.on("connect", () => {
-      console.log("Client: Connection made to server: ", socket.id);
-    });
+  socket.on("connect", () => {
+    logger.debug("Client: Connection made to server: ", socket.id);
+  });
 
-    socket.on("disconnect", () => {
-      console.log("Client: disconnected");
-    });
+  socket.on("disconnect", () => {
+    logger.debug("Client: disconnected");
+  });
 
-    socket.on("PING", () => {
-      console.log("Client PING", socket.id);
-      socket.emit("PONG", socket.id);
-    });
+  socket.on("PING", () => {
+    logger.debug("Client PING", socket.id);
+    socket.emit("PONG", socket.id);
+  });
 
-    socket.on("reminder", (reminder) => {
-      console.log("Client reminder: ", reminder.message);
-    });
+  socket.on("reminder", (reminder) => {
+    logger.debug("Client reminder: ", reminder.message);
+  });
 
-    socket.on("sign-in-ok", () => {
-      console.log("Client sign-in-ok");
-    });
+  socket.on("sign-in-ok", () => {
+    logger.debug("Client sign-in-ok");
+  });
 
-    socket.on("sign-in-error", () => {
-      console.error("Client sign-in-error");
-    });
+  socket.on("sign-in-error", () => {
+    logger.error("Client sign-in-error");
+  });
 
-    socket.on("sign-out-ok", () => {
-      console.log("Client sign-out-ok");
-    });
+  socket.on("sign-out-ok", () => {
+    logger.debug("Client sign-out-ok");
+  });
 
-    socket.on("sign-out-error", () => {
-      console.error("Client sign-out-error");
-    });
-  }
+  socket.on("sign-out-error", () => {
+    logger.error("Client sign-out-error");
+  });
   return socket;
 };
